e496dd656701e32dfd1cf6f81b77eed4a8f1be51
Vladimir Ipatov <vs@ipatov.net>
2014-01-14 02:55:01 +0400

    added denyhosts module

---
+++ modules/denyhosts/manifests/init.pp
@@ -0,0 +1,35 @@
+class denyhosts {
+
+	package { denyhosts: ensure => installed }
+
+	file { "/etc/denyhosts.conf":
+		owner   => root,
+		group   => root,
+		mode    => 644,
+		source  => "puppet:///modules/denyhosts/denyhosts.conf",
+		require => Package["denyhosts"],
+	}
+
+	file { "/etc/hosts.allow":
+		owner   => root,
+		group   => root,
+		mode    => 644,
+		source  => "puppet:///modules/denyhosts/hosts.allow",
+		require => Package["denyhosts"],
+	}
+
+	file { "/var/lib/denyhosts/allowed-hosts":
+		owner   => root,
+		group   => root,
+		mode    => 600,
+		source  => "puppet:///modules/denyhosts/allowed-hosts",
+		require => Package["denyhosts"],
+	}
+
+	service { denyhosts:
+		ensure => running,
+		hasrestart => true,
+		subscribe => File["/etc/denyhosts.conf"],
+	}
+
+}
