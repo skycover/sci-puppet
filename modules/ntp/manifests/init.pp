9ced286991a702445fd08930ba0eb13e962d4ee3
Vladimir Ipatov <vs@ipatov.net>
2014-01-14 02:40:25 +0400

    refactored common module, ntp class taked out to separate module, added puppet marks to fixed config files in all modules

---
+++ modules/ntp/manifests/init.pp
@@ -0,0 +1,17 @@
+class ntp {
+	package { 'chrony': ensure => installed }
+
+	file { '/etc/chrony/chrony.conf':
+		mode => 644, owner => root, group => root,
+		source => 'puppet:///modules/common/chrony.conf',
+		require => Package['chrony']
+	}
+
+	service { 'chrony':
+		hasrestart => true,
+		subscribe => File[ '/etc/chrony/chrony.conf' ],
+		require => [ Package['chrony'] ]
+	}
+}
+
+
