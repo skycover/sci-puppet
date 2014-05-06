331994796a4a0961b4ead8ec2490e24f11dca5d4
Vladimir Ipatov <vs@ipatov.net>
2014-05-07 00:58:46 +0400

    add init.d override for late zabbix-agent startup

--- modules/zabbix-agent/manifests/init.pp
+++ modules/zabbix-agent/manifests/init.pp
@@ -1,22 +1,39 @@
 class zabbix-agent($zabbix_server="zabbix.$domain") {
 
-        package {zabbix-agent:
-                ensure=> installed,
-                allowcdrom => true,
-        }
+	package {zabbix-agent:
+		ensure=> installed,
+		allowcdrom => true,
+	}
 
-        file { "/etc/zabbix/zabbix_agentd.conf":
-                owner => "root",
-                group => "root",
-                mode => 644,
-                content => template("zabbix-agent/zabbix_agentd.conf.erb"),
-                require => Package['zabbix-agent'],
-        }
+	file { "/etc/zabbix/zabbix_agentd.conf":
+		owner => "root",
+		group => "root",
+		mode => 644,
+		content => template("zabbix-agent/zabbix_agentd.conf.erb"),
+		require => Package['zabbix-agent'],
+	}
 
-        service { zabbix-agent:
-                hasrestart => true,
-                subscribe =>  File[ "/etc/zabbix/zabbix_agentd.conf" ],
-                require =>  File[ "/etc/zabbix/zabbix_agentd.conf" ],
-        }
+	# workaround for early agent start
+	if $operatingsystem == "Debian" {
+		file { "/etc/insserv/overrides/zabbix-agent":
+        	owner   => root,
+    	    group   => root,
+	        mode    => 644,
+	        source  => "puppet:///modules/zabbix-agent/zabbix-agent.override",
+		}
+	    exec { "insserv_zabbix-agent":
+			command => '/sbin/insserv zabbix-agent',
+			require => [File["/etc/insserv/overrides/zabbix-agent"],Package["zabbix-agent"],],
+			unless  => "/bin/ls /etc/rc2.d|/usr/bin/tail -4|/bin/grep zabbix",
+		}
+	}
+
+	service { zabbix-agent:
+		hasrestart => true,
+		subscribe =>  File[ "/etc/zabbix/zabbix_agentd.conf" ], 
+		require =>  File[ "/etc/zabbix/zabbix_agentd.conf" ], 
+	}
+
+
 
 }
