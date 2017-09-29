15c9a12018c63dc932454fed043c3b9cc7b68b68
Vladimir Ipatov <vs@ipatov.net>
2017-09-29 22:35:48 +0300

    approx: fix path in sci-genkey.sh

--- modules/approx/files/sci-genkey.sh
+++ modules/approx/files/sci-genkey.sh
@@ -2,7 +2,7 @@
 #
 # file is ruled by puppet. resistance is futile
 #
-export PATH=/usr/bin:/bin
+export PATH=/usr/bin:/bin:/usr/sbin:/sbin
 
 HOMEDIR=/etc/sci/gpg
 
@@ -26,8 +26,10 @@ gpg --homedir $HOMEDIR --no-options --batch --gen-key $HOMEDIR/sci-key-input
 if [ $? -eq 0 ]; then
   gpg --homedir /etc/sci/gpg/ --export >$HOMEDIR/sci.pub
   echo Key exported to puppet
+  killall rngd
+  exit 0
 else
   echo GPG Key generation aborted
+  killall rngd
   exit 1
 fi
-
